3473ac6af498258b706a6160b58fb708af0ef50b
Robert H <robertjameshardin@aol.com>
2019-03-27 13:16:11 -0500

    added homepage, new image, new themes, etc.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,33 @@
 $(document).ready(function() {
     $(".GifTastic").hide();
     $(".Office-Trivia").hide();
+    $(".wrapper").hide();
     const aboutMe = $(".aboutMe");
     aboutMe.hide();
     const contactInfo = $(".contactInfo");
     contactInfo.hide();
     const portfolio = $(".portfolio");
     portfolio.hide();
+    if ($(".wrapper")) {
     setTimeout(aboutDiv, 1000);
-    setTimeout(contactDiv, 2000);
-    setTimeout(portfolioDiv, 3000);
+    setTimeout(contactDiv, 3000);
+    setTimeout(portfolioDiv, 2000);
+    } else {
+        clearTimeout(aboutDiv);
+        clearTimeout(contactDiv);
+        clearTimeout(portfolioDiv);
+    }
     function aboutDiv() {
-        aboutMe.addClass("animated fadeInRight");
+        aboutMe.addClass("animated fadeInRight slow");
         aboutMe.show();
     };
     function contactDiv() {
-        contactInfo.addClass("animated fadeInRight");
+        contactInfo.addClass("animated fadeInUp slow");
         contactInfo.show()
     }
     setTimeout(projectSwap, 6000);  
     function portfolioDiv() {
-        portfolio.addClass("animated fadeInUp");
+        portfolio.addClass("animated fadeInRight slow");
         portfolio.show();
     }
     function projectSwap() {
@@ -31,11 +38,16 @@ $(document).ready(function() {
         setTimeout(function() {
             $(".GifTastic").hide();
             $(".Office-Trivia").show();
-        }, 8000);
+        }, 12000);
         setTimeout(function() {
             $(".Office-Trivia").hide();
             $(".Nerd-Lator").show();
-        }, 12000);
-        setTimeout(projectSwap, 16000);
+        }, 20000);
+        setTimeout(projectSwap, 28000);
     }
+    $(".aboutBtn").on("click", function() {
+        $(".introDiv").hide();
+        $(".wrapper").addClass("animated fadeIn slow");
+        $(".wrapper").show();
+    })
 });
